64630c5c163a7b5a2ed9b65e95f80dba0e188c10
saa23 <achmadgunarsaadi@gmail.com>
2023-10-24 23:21:55 +0700

    data types - checking

---
+++ data_types/1.data_types.py
@@ -0,0 +1,44 @@
+namaku = 'Djoko'
+
+x  = 10    # int
+y = 10.5   # float
+z = 4j    # complex
+
+is_larger = 10 > 5
+
+my_list = ['soto', 'mi goreng', 'sate']
+my_tuple = ('soto', 'mi goreng', 'sate')
+my_set = {'soto', 'mi goreng', 'sate', 'soto', 'sate', 'soto', 'sate', 'soto'}
+
+my_dict = {
+    "name": "Djoko",
+    "age": "25",
+    "occupation": "Marketing Analyst",
+}
+
+print(f'namaku = {namaku}')
+print(f'namaku data type is {type(namaku)}')
+
+print(f'x = {x}')
+print(f'x data type is {type(x)}')
+
+print(f'y = {y}')
+print(f'y data type is {type(y)}')
+
+print(f'z = {z}')
+print(f'z data type is {type(z)}')
+
+print(f'is_larger = {is_larger}')
+print(f'is_larger data type is {type(is_larger)}')
+
+print(f'my_list = {my_list}')
+print(f'my_list data type is {type(my_list)}')
+
+print(f'my_tuple = {my_tuple}')
+print(f'my_tuple data type is {type(my_tuple)}')
+
+print(f'my_set = {my_set}')
+print(f'my_set data type is {type(my_set)}')
+
+print(f'my_dict = {my_dict}')
+print(f'my_dict data type is {type(my_dict)}')
---
+++ data_types/README.md
@@ -0,0 +1,11 @@
+Here are primary data types in python:
+1. text
+   1. str (string)
+2. numbers
+    1. int (integer)
+    2. float (decimal number)
+    3. complex (numbers with real and imaginary parts)
+3. list
+4. tuples
+5. set
+6. dict (Dictionary)
