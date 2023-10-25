d515c61ca98eef3bf09c37d3bb7b83029908c8b4
achmad.saadi <achmad.saadi@mamiteam.com>
2023-10-25 11:13:02 +0700

    update data-types output

--- data_types/1.data_types.py
+++ data_types/1.data_types.py
@@ -19,26 +19,61 @@ my_dict = {
 print(f'namaku = {namaku}')
 print(f'namaku data type is {type(namaku)}')
 
+# namaku = Djoko
+# namaku data type is <class 'str'>
+
+
 print(f'x = {x}')
 print(f'x data type is {type(x)}')
+
+# x = 10
+# x data type is <class 'int'>
+
 
 print(f'y = {y}')
 print(f'y data type is {type(y)}')
 
+# y = 10.5
+# y data type is <class 'float'>
+
+
 print(f'z = {z}')
 print(f'z data type is {type(z)}')
+
+# z = 4j
+# z data type is <class 'complex'>
+
 
 print(f'is_larger = {is_larger}')
 print(f'is_larger data type is {type(is_larger)}')
 
+# is_larger = True
+# is_larger data type is <class 'bool'>
+
+
 print(f'my_list = {my_list}')
 print(f'my_list data type is {type(my_list)}')
+
+# my_list = ['soto', 'mi goreng', 'sate']
+# my_list data type is <class 'list'>
+
 
 print(f'my_tuple = {my_tuple}')
 print(f'my_tuple data type is {type(my_tuple)}')
 
+# my_tuple = ('soto', 'mi goreng', 'sate')
+# my_tuple data type is <class 'tuple'>
+
+
 print(f'my_set = {my_set}')
 print(f'my_set data type is {type(my_set)}')
 
+# my_set = {'soto', 'sate', 'mi goreng'}
+# my_set data type is <class 'set'>
+
+
 print(f'my_dict = {my_dict}')
 print(f'my_dict data type is {type(my_dict)}')
+
+# my_dict = {'name': 'Djoko', 'age': '25', 'occupation': 'Marketing Analyst'}
+# my_dict data type is <class 'dict'>
